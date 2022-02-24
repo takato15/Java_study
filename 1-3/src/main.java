d3d96ec29a9bd1839802885e8df79d4e31f94c8f
takato15 <gshawks21@gmail.com>
2022-02-25 01:17:29 +0900

    再代入

---
+++ 1-3/src/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+	public static void main(String[] args) {
+		int age;
+		age = 20;
+		System.out.println("私の年齢は" + age);
+		age = 31;
+		System.out.println("・・・いや、本当の年齢は" + age);
+	}
+
+}
