594de3d045a8546857342f3a66ded9162eb8c2ab
takato15 <gshawks21@gmail.com>
2022-02-25 15:58:29 +0900

    定数宣言

---
+++ 1-5/src/main.java
@@ -0,0 +1,17 @@
+
+public class main {
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		int pie = 5;
+		
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * PI);
+		System.out.println("パイの半径を倍にします");
+		
+		//再代入
+		PI = 10;
+				
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * PI);
+	}
+}
