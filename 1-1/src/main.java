fdb3957df75dac51e31e963e6d41d7e14134d4d6
takato15 <gshawks21@gmail.com>
2022-02-25 14:48:59 +0900

    ソースコードの基本構造

---
+++ 1-1/src/main.java
@@ -0,0 +1,9 @@
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("RPG:スッキリ魔王征伐");
+		System.out.println("ver.0.1 by 湊");
+		System.out.println("＜ただいま鋭意学習・制作中＞");
+		System.out.println("プログラムを終了します");
+	}
+}
